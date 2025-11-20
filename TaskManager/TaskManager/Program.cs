d7ca47ff165007342bd16e9deb41fbb219e1606c
quyvinh1 <vvinh8198@gmail.com>
2025-11-20 14:19:02 +0700

    Fix: Environment#2

--- TaskManager/TaskManager/Program.cs
+++ TaskManager/TaskManager/Program.cs
@@ -121,12 +121,12 @@ builder.Services.AddSwaggerGen(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
-    
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-app.UseSwagger();
-app.UseSwaggerUI();
+
 app.UseMiddleware<GlobalExceptionHandler>();
 app.UseHttpsRedirection();
 app.UseRouting();
