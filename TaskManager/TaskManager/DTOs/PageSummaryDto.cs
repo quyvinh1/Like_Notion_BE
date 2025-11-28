838104568caece9364c308aac1f8990f7c5759d2
quyvinh1 <vvinh8198@gmail.com>
2025-11-28 15:48:28 +0700

    Modified: Calendar

--- TaskManager/TaskManager/DTOs/PageSummaryDto.cs
+++ TaskManager/TaskManager/DTOs/PageSummaryDto.cs
@@ -7,6 +7,7 @@
         public bool HasChildren { get; set; }
         public int? ParentId { get; set; }
         public bool IsCompleted { get; set; }
+        public DateTime? StartDate { get; set; }
         public DateTime? DueDate { get; set; }
         public string? Icon { get; set; }
         public string Status { get; set; }
